58164d6ff29a313cd2b02fce3c574d9277d1e62c
j-explorer <jjovanovic33@gmail.com>
2023-04-18 18:47:51 +0100

    Domain name added

--- launch_app/app.js
+++ launch_app/app.js
@@ -5,12 +5,34 @@ const filter= document.getElementById('filter');
 const filterHeight = filter.getBoundingClientRect().height;
 
 
-window.addEventListener('scroll', () => {
+
+window.addEventListener('scroll', scrollRight);
+function scrollRight(){
   if (documentHeight >= viewportHeight-filterHeight) {
     filter.style.position='fixed';
   }
   else { 
     filter.style.position='';
   } 
-})
+ }
 
+ document.addEventListener('DOMContentLoaded', scrollRight);
+
+//collapsible filter characteristics
+const collapse = document.getElementsByClassName("collapsible");
+
+
+for (i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", collapseElement);
+  function collapseElement(){
+    const characteristic = this.nextElementSibling;
+    if (characteristic.style.height=== "0vh") {
+      characteristic.style.height = "40vh";
+    } else {
+      characteristic.style.height = "0vh";
+    }
+  };
+}
+
+
+document.addEventListener('DOMContentLoaded', collapseElement);
